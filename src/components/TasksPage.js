8bee51ffcb5efbc468759c07f715416e1415abb4
jordan <jordan@jordans-MacBook-Air.local>
2020-06-30 14:24:43 -0700

    finished UI on form

--- src/components/TasksPage.js
+++ src/components/TasksPage.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 
 const TasksPage = () => {
+    const [cardForm, showCardForm] = useState(false);
+
+    const formToggler = () => {
+        showCardForm(!cardForm)
+    }
     return(
-        <div>
-            <h1>TasksPage</h1>
+        <div className= 'container my-5'>
+            <div className= 'jumbotron py-3'>
+                <div className= 'row'>
+                    <div className= 'col-md-2'>
+                        <button className= 'btn btn-success m-3' onClick= {formToggler}>+</button>
+                    </div>
+                    <div className= 'col-md-10'>
+                        <h2 className= 'display-4 text-center text-uppercase'>
+                            Task Manager
+                        </h2>
+                    </div>
+                </div>
+                {cardForm && (
+                <form>
+                    <div className= 'form-group'>
+                        <input type= 'text' className= 'form-control' placeholder= 'task title'/> 
+                    </div>
+                    <div className= 'form-group'>
+                        <textarea type= 'text' className= 'form-control' placeholder= 'description'/> 
+                    </div>
+                    <button className= 'btn btn-primary' type= 'submit'>Submit</button> 
+                </form>
+                )}
+            </div>
         </div>
     )
 }
